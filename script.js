927fc1c14028f10a7f219a6d6b22b9f893b05893
Pete Sekesn <72456128+The-Spruce-Goose@users.noreply.github.com>
2020-12-04 00:03:55 -0500

    js algorithim and const URL

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+'use strict';
+
 //declare a variable for LOC API URL
+const searchURL = 'https://www.loc.gov/collections/chronicling-america/';
 
 //declare a variable for Quote API URL
-
+const quoteURL = 'https://qvoca-bestquotes-v1.p.rapidapi.com/quote/?genre=history';
 //Declare a variable for Quote API Key
 
-//Take user entered text and add to LOC API
-
-//Take user selected max result and add to LOC API
+//Let Submit button append text and max result LOC API URL
 
 //Send user created LOC URL to API 
+
+//Send API Result Images to <ul>
+
+//Link results image to LOC 
+
+//Upon new search submission, clear previous results 
+
+//On page load, generate QOD 
+//On submit click, generate new QOD onto results page 
