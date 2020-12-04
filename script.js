3241ba8445c71ca107d660d9034057d918ecdaaa
Pete Sekesn <72456128+The-Spruce-Goose@users.noreply.github.com>
2020-12-03 22:45:54 -0500

    .hidden styling.

---
+++ script.js
@@ -0,0 +1,11 @@
+//declare a variable for LOC API URL
+
+//declare a variable for Quote API URL
+
+//Declare a variable for Quote API Key
+
+//Take user entered text and add to LOC API
+
+//Take user selected max result and add to LOC API
+
+//Send user created LOC URL to API 
