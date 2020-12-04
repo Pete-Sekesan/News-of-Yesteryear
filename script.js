c99b41421286a94f80fb937b615b5f043f41499e
Pete Sekesn <72456128+The-Spruce-Goose@users.noreply.github.com>
2020-12-04 11:27:21 -0500

    added format QueryParams  function

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ const searchURL = 'https://www.loc.gov/collections/chronicling-america/';
 //declare a variable for Quote API URL
 const quoteURL = 'https://qvoca-bestquotes-v1.p.rapidapi.com/quote/?genre=history';
 //Declare a variable for Quote API Key
-
+const apiKey = 'eedb904fd7mshdbabd39c1ef8444p1e448cjsn4120f5877999'
 //Let Submit button append text and max result LOC API URL
+//Format the query results for use in search URL
+function formatQueryParams(params){
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)} = ${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
 
 //Send user created LOC URL to API 
 
