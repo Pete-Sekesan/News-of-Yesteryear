8db326bd75166f2028e6694233b66cfc425759dc
Pete Sekesn <72456128+The-Spruce-Goose@users.noreply.github.com>
2020-12-06 10:30:50 -0500

    mvp edits complete

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const searchURL = 'https://www.loc.gov/collections/chronicling-america/';
 //declare a variable for Quote API URL
 const quoteURL = 'https://qvoca-bestquotes-v1.p.rapidapi.com/quote/?genre=history';
 //Declare a variable for Quote API Key
-const apiKey = 'eedb904fd7mshdbabd39c1ef8444p1e448cjsn4120f5877999'
-//Let Submit button append text and max result LOC API URL
 
 //Format the query results for use in search URL
 function formatQueryParams(params){
@@ -22,23 +20,21 @@ function getNews(topic, limit=5){
         q: topic,
         c: limit
     };
-
-const queryString = formatQueryParams(params);
-const url = searchURL + '?' + queryString + '&fo=json';
-console.log(url)
-
-fetch(url)
-.then(response =>{
-    if (response.ok){
-        return response.json();
-    }
-    throw new Error(response.statusText);
-})
-.then(responseJson => displayResults(responseJson))
-.catch(error => {
-    $('error-message').text(`Oops! Something went wrong: ${error.message}`);
-
-});
+    const queryString = formatQueryParams(params);
+    const url = searchURL + '?' + queryString + '&fo=json';
+    console.log(url)
+    
+    fetch(url)
+    .then(response =>{
+        if (response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => {
+        $('error-message').text(`Oops! Something went wrong: ${error.message}`);
+    });
 }
 
 
@@ -54,20 +50,18 @@ function displayResults(responseJson){
         let description = responseJson.results[i].description;
         let image = responseJson.results[i].image_url[1];
         let url = responseJson.results[i].id;
+        let date = responseJson.results[i].date;
         $('#results-list').append(
-            `<li><h2> Newspaper Source: ${title}</h2></li>
-            <li><a href="${url}" target="_blank"><img src="${image}" alt="Digitally Scanned Newspaper"></a></li>
-            <li><h3> Description: ${description} </h3></li>
-            `
-        )
-        console.log('results appended');
-
+            `<li><h2> Newspaper Source: ${title}</h2>
+            <a href="${url}" target="_blank"><img src="${image}" alt="Digitally Scanned Newspaper"></a>
+            <p> ${date}
+            <p> Description: ${description} </p></li>`
+            )
+            console.log('results appended');
+        }
+        $('#results').removeClass('hidden')
     }
-    $('#results').removeClass('hidden')
-}
-    //Link results image to LOC 
-
-//Upon new search submission, clear previous results 
+   
 
 
 //Get History QOD
@@ -76,13 +70,27 @@ fetch("https://qvoca-bestquotes-v1.p.rapidapi.com/quote?message=fire", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "eedb904fd7mshdbabd39c1ef8444p1e448cjsn4120f5877999",
-		"x-rapidapi-host": "qvoca-bestquotes-v1.p.rapidapi.com"
-	}
+        "x-rapidapi-host": "qvoca-bestquotes-v1.p.rapidapi.com"
+    }
 })
+.then(responseQuote =>{
+    if (responseQuote.ok){
+        return responseQuote.json();
+    }
+    throw new Error(responseQuote.statusText);
+})
+.then(responseQuoteJson => displayQuote(responseQuoteJson))
+.catch(error => {
+    $('error-message').text(`Oops! Something went wrong: ${error.message}`);
+});
 }
 
 //display historical quote on page load and on each submit click
-
+function displayQuote(responseQuoteJson){
+    $('#history-quote').empty();
+    $('#history-quote').append(
+        `<p> ${responseQuoteJson.message} </p>`);
+}
 
 //create a watchForm to tie in above functions and variables
 function watchForm(){
@@ -93,7 +101,6 @@ function watchForm(){
         const limit = $('#max-results').val();
         getNews(topic, limit);
         getQuote();
-    }
-    );
+    });
 }
 $(watchForm)
